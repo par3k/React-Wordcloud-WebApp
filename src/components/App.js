66d436d6e5c1153511de244fe932010fbdfef123
Park Hoijai <hoijae0194@naver.com>
2020-07-20 17:58:38 +0900

    React Project

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <h3>Hello World</h3>
+            </div>
+        );
+    }
+}
+
+export default App;
