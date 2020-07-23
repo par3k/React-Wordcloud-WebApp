e9a1bea3723a3f49dd923ae17b48d7ee9df8b200
Park Hoijai <hoijae0194@naver.com>
2020-07-23 13:59:32 +0900

    7. Upload the TextFile and Read

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import AppShell from './AppShell';
 import Home from './Home';
 import Texts from './Texts';
 import Words from './Words';
+import Detail from './Detail';
 
 class App extends React.Component{
     render(){
@@ -15,6 +16,7 @@ class App extends React.Component{
                         <Route exact path="/" component={Home}/>
                         <Route exact path="/texts" component={Texts}/>
                         <Route exact path="/words" component={Words}/>
+                        <Route exact path="/detail/:textID" component={Detail}/>
                     </div>
                 </AppShell>
             </Router>
